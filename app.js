57967042f54c091039f072bd0409d3be46063df4
unknown <you@example.com>
2024-09-04 08:50:58 -0700

    Java Assignment-1

---
+++ app.js
@@ -0,0 +1,69 @@
+// // Chapter 1
+// // Q1
+
+// alert("Abdul")
+// alert("Ahad")
+// alert("ahada00077@gmail.com")
+// alert("0317 3820880")
+// alert("Zee123")
+
+// Q2
+
+// alert("You're learning JavaScript!")
+
+// Q3
+
+// alert("Welcome To JS Land")
+
+// Chapter 2
+
+// Q1
+
+// var fullName ;
+
+// Q2
+
+// var last_name=;
+// last_name="Abdul Ahad";
+
+// Q3
+
+// var teamName = "Ahad's Team"
+// alert(teamName)
+
+// Q4
+
+// var bestMan = "Charlie"
+// bestMan = "John Doe"
+
+// Chapter 3
+
+// Q1
+
+// var caseQty =""
+
+// Q2
+
+// caseQty="144"
+
+// Q3
+
+// var num ="9"
+
+// Q4
+
+// var add
+// add="5+10"
+
+// Q5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal)
+
+// Q6
+
+// var num = 7;
+// num= num+5
+// console.log(num)
